ea693cc6b5fab77598e23d7e5d36031560f43c73
Arthur L <18040415+alouradou@users.noreply.github.com>
2024-10-18 20:47:23 +0200

    Added config for Eleventy 3.0

---
+++ config/assets.js
@@ -0,0 +1,21 @@
+// const postCSSConfig = require("eleventy-postcss-extension");
+
+export default function (eleventyConfig) {
+
+  // eleventyConfig.addPlugin(postCSSConfig);
+
+  // garder le node_modules des assets, ignorer les autres
+  eleventyConfig.addPassthroughCopy("src/assets/node_modules");
+  eleventyConfig.ignores.add("src/!(assets)/**/node_modules");
+
+  // images 
+  eleventyConfig.addPassthroughCopy("src/**/!(node_modules)/**/*.{jpg,png,ico,pdf,svg,gif}");
+
+  // videos
+  eleventyConfig.addPassthroughCopy("src/**/!(node_modules)/**/*.{webm,mov,mp4,ogv}");
+  
+  // data
+  eleventyConfig.addPassthroughCopy("src/**/!(node_modules)/**/*.{txt,edi,csv,json,pdf,zip}");
+
+};
+
---
+++ config/markdown/index.js
@@ -0,0 +1,40 @@
+import markdownIt from "markdown-it"
+import markdownItAttrs from "markdown-it-attrs"
+import markdownItMultimdTable from "markdown-it-multimd-table"
+import eleventyMdSyntaxHighlight from "@pborenstein/eleventy-md-syntax-highlight"
+
+// import shortcodes from "./shortcodes/index.js"
+
+export default async function(eleventyConfig) {
+
+    let markdownItLibrary = markdownIt({
+        html: true,
+        breaks: false,
+        linkify: true
+    })
+
+    markdownItLibrary
+        .use(markdownItAttrs)
+        .use(markdownItMultimdTable, {
+            multiline: true,
+            rowspan: true,
+            headerless: true,
+            multibody: true,
+            aotolabel: true,
+        });
+
+
+    eleventyConfig.addPlugin(eleventyMdSyntaxHighlight,
+        {showLineNumbers: false}
+    )
+
+    // eleventyConfig.addPlugin(require("eleventy-plugin-mathjax"));
+
+    eleventyConfig.addFilter("md", function (content = "") {
+        return markdownItLibrary.render(content);
+    });
+
+    eleventyConfig.setLibrary("md", markdownItLibrary)
+
+    // shortcodes(eleventyConfig);
+};
